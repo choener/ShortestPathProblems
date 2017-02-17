57ddb821a5ec66e31e4fa7a876bf175764b60300
Christian Hoener zu Siederdissen <choener@bioinf.uni-leipzig.de>
2017-02-17 20:09:07 +0100

    added combined I/O/C edge prob grammar

---
+++ ShortestPath/SHP/EdgeProbIO.hs
@@ -0,0 +1,33 @@
+
+module ShortestPath.SHP.EdgeProbIO where
+
+import ADP.Fusion.Core
+import FormalLanguage
+
+
+
+[formalLanguage|
+Verbose
+Grammar: EdgeProb
+N: F
+N: L
+N: Z
+T: s
+T: k
+S: Z
+-- inside
+F -> mpty <<< ε       -- empty set
+F -> node <<< F s     -- single node
+F -> edge <<< F k     -- edge k
+-- outside
+L -> mpty <<< ε       -- full set
+L -> node <<< L s     -- single node missing
+L -> edge <<< L k     -- edge k removed
+-- combine inside and outside
+Z -> fini <<< L k F   -- edges bracketed by a Last and a First set
+//
+Emit: EdgeProb
+|]
+
+makeAlgebraProduct ''SigEdgeProb
+
